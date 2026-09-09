7f39435d9e5d36d5079a9b950a16e24548da6363
florescentro1234-byte <florescentro1234@gmail.com>
2026-09-08 20:59:24 -0700

    Updated config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,5 @@
-// ── Configurable endpoint (Google Apps Script) ───────────────────────────
-export const SCRIPT_URL =
-  'https://script.google.com/macros/s/AKfycbz9CLDQqhMiYTOuO_eC0Wiy8DL2pLBwMK3fZ59aNF98_mSyuJLsK5Brgbjgs98cUubH_g/exec';
+// - Configurable endpoint (Google Apps Script) -
+export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz9CLDQqhMlYTQuO_eCOWiy8DL2pLBwMK3fZ59aNF98_mSyuJLsK5Brgbjgs98cUubH_g/exec';
 
 export const MAX_CUPOS = 5;
 export const VIGENCIA_DIAS = 30;
